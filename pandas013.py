0014458586622ca19ae728f23aec9256fab8066c
Aruna2341 <118328745+Aruna2341@users.noreply.github.com>
2022-11-16 09:28:26 +0530

    Add files via upload

---
+++ pandas013.py
@@ -0,0 +1,17 @@
+import pandas as pd
+df = pd.read_csv('online-retail.csv')
+print(df)
+df['Lineprice'] = df['Quantity'] * df['UnitPrice']
+df.head()
+
+print(df)
+
+df_customers =df.groupby('CustomerId').agg(
+    orders=('InvoiceNo', 'nunique'),
+    skus=('StockCode', 'nunique'),
+    quantity=('Quantity', 'sum'),
+    revenue=('LinePrice', 'sum'),
+).reset_index()
+
+df_customers.head()
+print(df_customers)
